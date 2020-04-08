c3cd6beaa0d9f74f6e74a126890438a89efb183b
Drew Willey <andwilley@gmail.com>
2020-04-08 02:02:30 +0000

    simplify

--- app/Main.hs
+++ app/Main.hs
@@ -12,11 +12,11 @@ import           Web.Scotty.Internal.Types
 
 main :: IO ()
 main =
-    scotty 8080
-        $ post "/notes" $ do
-            apikey <- param "apikey"
-            case apikey of
-                _ | Set.member apikey apikeys -> raw =<< (liftIO . runApi =<< body)
-                _      -> do
-                    status status500
-                    html $ mconcat ["<h1>500 Internal Server Error</h1>", "Bad API Key: ", apikey]
+  scotty 8080
+    $ post "/notes" $ do
+      apikey <- param "apikey"
+      case apikey of
+        _ | Set.member apikey apikeys -> raw =<< (liftIO . runApi =<< body)
+        _      -> do
+          status status500
+          html $ mconcat ["<h1>500 Internal Server Error</h1>", "Bad API Key: ", apikey]
